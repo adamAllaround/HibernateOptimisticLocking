8dcc747716d07cfcbe08ab54c335099a4ececfd7
adamAllaround <adam@allaroundjava.com>
2019-05-06 17:30:54 +0200

    Adding Car Entity

---
+++ src/main/java/com/allaroundjava/model/Car.java
@@ -0,0 +1,44 @@
+package com.allaroundjava.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+@Entity
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String make;
+
+    @Column(nullable = false)
+    private String model;
+
+    @Version
+    private int version;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
